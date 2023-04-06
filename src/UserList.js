decf7ddbb047818bbad7827cf61c7299f512d821
Amani Magloire <debabel1hackamani@gmail.com>
2023-04-06 12:07:23 +0000

    last commit

---
+++ src/UserList.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap";
+
+function UserList() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setListOfUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Liste des utilisateurs</h1>
+      <ul className="list-group">
+        {listOfUsers.map((user) => (
+          <li key={user.id} className="list-group-item">
+            {user.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
